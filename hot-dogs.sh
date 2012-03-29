55125f8d7922660c621c2ad17c7fa6e0b4c33b00
Hanna Kalinowska <h.kalinowska@nature.com>
2012-03-29 15:40:19 +0100

    Look, multiple lines!

--- hot-dogs.sh
+++ hot-dogs.sh
@@ -1,2 +1,17 @@
 #! /bin/sh
-curl http://www.eat.st/kings-cross/kings-cross-calendar.php 2>&1 | grep -oP "eat.st at King's Boulevard on `date +"%e(?:st|nd|rd|th) %h %Y"`|Big Apple Hot Dogs|eat.st at King's Boulevard on `date -v+1d +"%e(?:st|nd|rd|th) %h %Y"`" | head -n 2 | tr "\\n" " " | if [[ -z $(grep -P "`date +"%e(?:st|nd|rd|th) %h %Y"`") ]]; then echo "No eat.st today!"; elif [[ -n $(grep -P "Big Apple Hot Dogs") ]]; then echo "YES!"; else echo "No :("; fi
+stall="Big Apple Hot Dogs"
+
+result=`curl http://www.eat.st/kings-cross/kings-cross-calendar.php 2>&1`
+today=`date +"%e(?:st|nd|rd|th) %h %Y"`
+tomorrow=`date -v+1d +"%e(?:st|nd|rd|th) %h %Y"`
+result=`echo $result | grep -oP "eat.st at King's Boulevard on $today|$stall|eat.st at King's Boulevard on $tomorrow" | head -n 2 | tr "\\n" " "`
+
+if [[ -z $(echo $result | grep -P "$today") ]]
+then
+  echo "No eat.st today!"
+elif [[ -n $(echo $result | grep -P "$stall") ]]
+then
+  echo "YES!"
+else
+  echo "No :("
+fi
