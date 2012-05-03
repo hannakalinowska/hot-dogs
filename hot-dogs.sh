3ba221e4dc5c04f778d2378a086e96876e374237
Hanna Kalinowska <hanna.kalinowska@gmail.com>
2012-05-03 20:40:06 +0100

    Another try

--- hot-dogs.sh
+++ hot-dogs.sh
@@ -4,14 +4,11 @@ stall="Big Apple Hot Dogs"
 result=`curl http://www.eat.st/kings-cross/kings-cross-calendar.php 2>&1`
 today=`date +"%e(?:st|nd|rd|th) %h %Y"`
 tomorrow=`date -v+1d +"%e(?:st|nd|rd|th) %h %Y"`
-result=`echo $result | grep -oP "eat.st at King's Boulevard on $today|$stall|eat.st at King's Boulevard on $tomorrow" | head -n 2 | tr "\\n" " "`
-
-if [[ -z $(echo $result | grep -P "$today") ]]
-then
-  echo "No eat.st today!"
-elif [[ -n $(echo $result | grep -P "$stall") ]]
-then
-  echo "YES!"
-else
-  echo "No :("
-fi
+result=`echo $result | grep -oP "eat.st at King's Boulevard on $today|$stall|eat.st at King's Boulevard on $tomorrow" \
+  | head -n 2 \
+  | tr "\\n" " " \
+  | sed -E "s/^eat.st at King's Boulevard on $today $stall\s*$/YES/" \
+  | sed -E "s/^eat.st at King's Boulevard on $today eat.st at King's Boulevard on $tomorrow\s*$/No/" \
+  | sed -E "s/^eat.st at .+$/No eat.st today/" \
+  `
+echo $result
