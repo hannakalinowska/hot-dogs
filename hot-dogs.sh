061681be55ae94a494a2ec945bceea8e16839e6f
Hanna Kalinowska <h.kalinowska@nature.com>
2012-05-04 12:05:14 +0100

    OS X just has to be different

--- hot-dogs.sh
+++ hot-dogs.sh
@@ -2,13 +2,13 @@
 stall="Big Apple Hot Dogs"
 
 result=`curl http://www.eat.st/kings-cross/kings-cross-calendar.php 2>&1`
-today=`date +"%e(?:st|nd|rd|th) %h %Y"`
-tomorrow=`date -v+1d +"%e(?:st|nd|rd|th) %h %Y"`
-result=`echo $result | grep -oP "eat.st at King's Boulevard on $today|$stall|eat.st at King's Boulevard on $tomorrow" \
+today=`date +"%e(st|nd|rd|th) %h %Y"`
+tomorrow=`date -v+1d +"%e(st|nd|rd|th) %h %Y"`
+result=`echo $result | grep -oP "eat.st at King's Boulevard on ?$today|$stall|eat.st at King's Boulevard on ?$tomorrow" \
   | head -n 2 \
   | tr "\\n" " " \
-  | sed -E "s/^eat.st at King's Boulevard on $today $stall\s*$/YES/" \
-  | sed -E "s/^eat.st at King's Boulevard on $today eat.st at King's Boulevard on $tomorrow\s*$/No/" \
+  | sed -E "s/^eat.st at King's Boulevard on ?$today $stall.*$/YES/g" \
+  | sed -E "s/^eat.st at King's Boulevard on ?$today eat.st at King's Boulevard on ?$tomorrow.*$/No/" \
   | sed -E "s/^eat.st at .+$/No eat.st today/" \
   `
 echo $result
